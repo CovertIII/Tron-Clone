ebcbcda5d7bace4a3fd794d3d8b55ea335064fd9
Bill Covert <billy.covert@gmail.com>
2011-01-13 22:43:43 -0500

    modified client.  much more work to be done to it.

--- client.c
+++ client.c
@@ -19,16 +19,19 @@
 #define COMMAND 4
 
 typedef struct clienttype{
+	ENetPeer * server;
+	ENetHost * client;
 	int game_state;
 	int game_mode;
 	arena c_game;
 	chat c_chat;
 	user c_users;
 	char mbuf[500];
+	int mbuf_num;
 	double timer;
 } clienttype;
 
-client client_init(void){
+client client_init(ENetHost * client){
 	client clnt;
 
 	clnt = (client)malloc(sizeof(clienttype));
@@ -40,6 +43,7 @@ client client_init(void){
 	clnt->c_chat = chat_init();
 	clnt->c_users = user_init();	
 	clnt->timer = 0;
+	mbuf_num = 0;
 
 	return clnt;
 }
@@ -58,13 +62,81 @@ void client_update(client clnt, double dt){
 }
 
 void client_render(client clnt){
-	
+	char *buf;
+	chat_render(clnt->c_chat, 0);
+	switch(clnt->game_mode){
+		case NOT_CONNECTED:	
+			glPushMatrix();
+			glLoadIdentity();
+			glColor3f(0, 1, 1);
+			renderBitmapString(10, glutGet(GLUT_WINDOW_HEIGHT) - 3, GLUT_BITMAP_TIMES_ROMAN_10, "Please enter the server you's like to connect to:");
+			glPopMatrix();
+			glPushMatrix();
+			glLoadIdentity();
+			glColor3f(0, 1, 1);
+			renderBitmapString(10, glutGet(GLUT_WINDOW_HEIGHT)-15, GLUT_BITMAP_TIMES_ROMAN_10, clnt->mbuf);
+			glPopMatrix();
+			break;
+		case MESSAGE:
+			glPopMatrix();
+			glPushMatrix();
+			glLoadIdentity();
+			glColor3f(0, 1, 1);
+			renderBitmapString(10, glutGet(GLUT_WINDOW_HEIGHT)-15, GLUT_BITMAP_TIMES_ROMAN_10, clnt->mbuf);
+			glPopMatrix();
+			break;
+		case NAME:
+			glPushMatrix();
+			glLoadIdentity();
+			glColor3f(0, 1, 1);
+			renderBitmapString(10, glutGet(GLUT_WINDOW_HEIGHT) - 3, GLUT_BITMAP_TIMES_ROMAN_10, "What would you like your name to be?");
+			glPopMatrix();
+			glPushMatrix();
+			glLoadIdentity();
+			glColor3f(0, 1, 1);
+			renderBitmapString(10, glutGet(GLUT_WINDOW_HEIGHT)-15, GLUT_BITMAP_TIMES_ROMAN_10, clnt->mbuf);
+			glPopMatrix();
+			break;
+	}
 }
 
 void client_keys(client clnt, unsigned char key){
+	ENetPacket * packet;
+	ENetAddress address;
+	ENetEvent event;
+	char disp [100];
+
 	switch(clnt->game_mode){
 		case NOT_CONNECTED:
+			if(type(clnt, key)){
+				clnt->mbuf[clnt->mbuf_num] = NULL;
+			
+				enet_address_set_host (& address, clnt->mbuf);
+				address.port = 5001;
 
+				/* Initiate the connection. The third argument is the number of channels, the 4th is user supplied data.*/
+				clnt->server = enet_host_connect (client, & address, 4, 0);    
+
+				if (clnt->server == NULL){
+					fprintf (stderr,"No available peers for initiating an ENet connection.\n");
+					exit (EXIT_FAILURE);
+				}
+
+				/* Wait up to 5 seconds for the connection attempt to succeed. */
+				if (enet_host_service (clnt->client, & event, 5000) > 0 && event.type == ENET_EVENT_TYPE_CONNECT){
+					sprintf(disp, "Connection to %s succeeded.\n", clnt->mbuf);
+					printf("Connection to %s succeeded.\n", clnt->mbuf);
+					chat_add_message(clnt->c_chat, "Client", disp);
+					clnt->game_mode = NORMAL;	
+					clnt->mbuf_num = 0;
+					clnt->mbuf[0] = NULL;
+				}
+				else{
+					enet_peer_reset (clnt->server);
+					sprintf(disp, "Connection to %s failed.", trans);
+					printf("Connection to %s failed.", trans);
+				}
+			}
 			break;
 		case MESSAGE:
 
@@ -84,21 +156,21 @@ void client_free(client clnt){
 }
 
 
-void type(unsigned char key) 
+int type(client clnt, unsigned char key) 
 {
 	int i;
 	if(key==10 || key==13){
-		buf[buf_num] = NULL;
-		strcpy(trans,buf);
-		buf_num=0;
-		tran_in = 1;
-		message_mode = OFF;
+		return 1;
 	}
 	else if(key>=1 && key < 127 && buf_num < 97){
-		buf[buf_num]=key;
+		clnt->mbuf[clnt->mbuf_num]=key;
 		buf_num++;
+		clnt->mbuf[clnt->mbuf_num+1] = NULL;
+		return 0;
 	}
-	else if (key == 127 && buf_num>0) {
-		buf_num--;
+	else if (key == 127 && clnt->mbuf_num>0) {
+		clnt->mbuf_num--;
+		clnt->mbuf[clnt->mbuf_num+1] = NULL;
+		return 0;
 	}
 }
